22c6b931296ffe6f04933b1a96a7692be82080f0
AnteVuletic <antevuletic1995@gmail.com>
2019-12-08 22:13:34 +0100

    Fixes to configuration

--- src/main/java/org/authentication/App.java
+++ src/main/java/org/authentication/App.java
@@ -1,6 +1,5 @@
 package org.authentication;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        Flyway flyway = Flyway.configure().dataSource("jdbc:postgresql://localhost:5432/postgres", "admin", "1234").load();
-        flyway.migrate();
         SpringApplication.run(App.class, args);
     }
 }
--- src/main/resources/db/migration/V1_1__create_users_security_profiles_and_claims.sql
+++ src/main/resources/db/migration/V1_1__create_users_security_profiles_and_claims.sql
@@ -15,9 +15,9 @@ create table authentication.Users (
 	UserId 		        varchar(50),
 	Email 		        varchar(30) not null,
 	Password	        varchar(50) not null,
-	FirstName					varchar(50) not null,
-	LastName					varchar(50) not null,
-	DateOfBirth				date,
+	FirstName			varchar(50) not null,
+	LastName			varchar(50) not null,
+	DateOfBirth			date,
 	IsActive	        bit,
 	SecurityProfileId   integer references  authentication.SecurityProfiles(SecurityProfileId),
 	primary key (UserId)
@@ -26,6 +26,6 @@ create table authentication.Users (
 create table authentication.Claims(
 	ClaimId 		serial,
 	UserId			varchar(50) references authentication.Users(UserId),
-	ResourceId	varchar(50) not null,
+	ResourceId	    varchar(50) not null,
 	primary key (ClaimId)
 )
