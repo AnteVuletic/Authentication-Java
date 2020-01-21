bd5bf686efcecf02a0f8b299e753983359581d9b
mluketin <mluketin@STUDENT06>
2020-01-21 22:03:34 +0100

    Done with profile panel

--- src/main/frontend/src/screens/Admin/Profiles.js
+++ src/main/frontend/src/screens/Admin/Profiles.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAllUserClaims } from '../../services/common';
+import { Accordion } from "react-bootstrap";
+import { ScreenContainer } from "../index.styled";
 
 const Profiles = () => {
+  const [profiles, setProfiles] = useState([]);
+
+  useEffect(() => {
+    getAllUserClaims(({data}) => {
+      setProfiles(data);
+    });
+  });
+
+  return (
+    <ScreenContainer>
+      <Accordion>
+        {profiles.map((profile, index) => {
+          return <span>{profile}</span>;
+        })}
+      </Accordion>
+    </ScreenContainer>
+  );
+
+
+
+
+
+
+
+
+
   return <div>profiles</div>;
 };
+
+
+
+
+
+
 
 export default Profiles;
